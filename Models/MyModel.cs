0463b71d69adc05a1af02eb1dc65586e1b276bd9
Mark Deanil Vicente <markdeanilvicente@gmail.com>
2017-05-28 19:07:18 +0800

    added json property

--- Models/MyModel.cs
+++ Models/MyModel.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ConsoleAppTest
 {
     public class MyModel
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
-
+        [JsonProperty("firstname")]
         public string Firstname { get; set; }
-
+        [JsonProperty("lastname")]
         public string Lastname { get; set; }
     }
 }
